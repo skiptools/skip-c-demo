92e6c2132d1a3b49f7c790c36ea066f49072cb47
marcprux <mwp1@cornell.edu>
2024-01-12 20:39:35 -0500

    Update assembly sample

--- Sources/LibCLibrary/src/demo.c
+++ Sources/LibCLibrary/src/demo.c
@@ -20,31 +20,16 @@ long add_with_assembly(long x, long y) {
     long result;
 
     #ifdef __i386__ // Intel 32-bit
-        asm volatile (
-            "add %1, %0"
-            : "=r" (result)
-            : "r" (y), "0" (x)
-        );
+    __asm__ ("add %1, %0" : "=r" (result) : "r" (y), "0" (x));
     #elif __x86_64__ // Intel 64-bit
-        asm volatile (
-            "add %1, %0"
-            : "=r" (result)
-            : "r" (y), "0" (x)
-        );
+    __asm__ ("addq %1, %0;" : "=r" (result) : "r" (y), "0" (x));
     #elif __arm__ // ARM 32-bit
-        asm volatile (
-            "adds %0, %1, %2"
-            : "=r" (result)
-            : "r" (x), "r" (y)
-        );
+    __asm__ ("adds %0, %1, %2" : "=r" (result) : "r" (x), "r" (y));
     #elif __aarch64__ // ARM 64-bit
-        asm volatile (
-            "add %0, %1, %2"
-            : "=r" (result)
-            : "r" (x), "r" (y)
-        );
+    __asm__ ("add %0, %1, %2" : "=r" (result) : "r" (x), "r" (y));
+    #elif __riscv_64 // RISC-V 64-bit (future)
+    __asm__ ("add %0, %1, %2" : "=r" (result) : "r" (x), "r" (y));
     #else
-        // TODO: RISC V
         #error "Unsupported architecture"
     #endif
 
