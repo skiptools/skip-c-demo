0ed46115be4a85d997474ca3ee2487fa80ee2ae7
marcprux <mwp1@cornell.edu>
2024-01-13 08:50:15 -0500

    Update assembly sample

--- Sources/LibCLibrary/src/demo.c
+++ Sources/LibCLibrary/src/demo.c
@@ -19,14 +19,17 @@ double demo_compute(int n, double a, double b) {
 long add_with_assembly(long x, long y) {
     long result;
     #ifdef __i386__ // Intel 32-bit
-    asm("add %1, %0" : "=r" (result) : "r" (x), "0" (y));
+    //asm("add %1, %0" : "=r" (result) : "r" (x), "0" (y));
+    return x + y;
     #elif __x86_64__ // Intel 64-bit
     //asm("add %1, %0" : "=r" (result) : "r" (x), "0" (y));
     return x + y;
     #elif __arm__ // ARM 32-bit
-    asm("adds %0, %1, %2" : "=r" (result) : "r" (x), "r" (y));
+    //asm("adds %0, %1, %2" : "=r" (result) : "r" (x), "r" (y));
+    return x + y;
     #elif __aarch64__ // ARM 64-bit
-    asm("add %0, %1, %2" : "=r" (result) : "r" (x), "r" (y));
+    //asm("add %0, %1, %2" : "=r" (result) : "r" (x), "r" (y));
+    return x + y;
     #elif __riscv_64 // RISC-V 64-bit (future)
     asm("add %0, %1, %2" : "=r" (result) : "r" (x), "r" (y));
     #else
