d80746d776e7fb927999a94c4d91c8ad26312352
marcprux <mwp1@cornell.edu>
2024-01-12 21:05:34 -0500

    Update assembly sample

--- Sources/LibCLibrary/src/demo.c
+++ Sources/LibCLibrary/src/demo.c
@@ -20,17 +20,17 @@ long add_with_assembly(long x, long y) {
     long result;
 
     #ifdef __i386__ // Intel 32-bit
-    __asm__ ("add %1, %0" : "=r" (result) : "r" (y), "0" (x));
+    __asm__ volatile ("addl %1, %0" : "=r" (result) : "r" (x), "0" (y));
     #elif __x86_64__ // Intel 64-bit
-    __asm__ ("addq %1, %0;" : "=r" (result) : "r" (y), "0" (x));
+    __asm__ volatile ("addq %1, %0;" : "=r" (result) : "r" (x), "0" (y));
     #elif __arm__ // ARM 32-bit
-    __asm__ ("adds %0, %1, %2" : "=r" (result) : "r" (x), "r" (y));
+    __asm__ volatile ("adds %0, %1, %2" : "=r" (result) : "r" (x), "r" (y));
     #elif __aarch64__ // ARM 64-bit
-    __asm__ ("add %0, %1, %2" : "=r" (result) : "r" (x), "r" (y));
+    __asm__ volatile ("add %0, %1, %2" : "=r" (result) : "r" (x), "r" (y));
     #elif __riscv_64 // RISC-V 64-bit (future)
-    __asm__ ("add %0, %1, %2" : "=r" (result) : "r" (x), "r" (y));
+    __asm__ volatile ("add %0, %1, %2" : "=r" (result) : "r" (x), "r" (y));
     #else
-        #error "Unsupported architecture"
+    #error "Unsupported architecture"
     #endif
 
     return result;
