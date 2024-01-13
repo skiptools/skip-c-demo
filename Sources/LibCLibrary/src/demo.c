2c04c11f2ece14fee50af9ffdd8899b43a2bea96
marcprux <mwp1@cornell.edu>
2024-01-12 22:22:34 -0500

    Update assembly sample

--- Sources/LibCLibrary/src/demo.c
+++ Sources/LibCLibrary/src/demo.c
@@ -20,7 +20,7 @@ long add_with_assembly(long x, long y) {
     long result;
     #ifdef __i386__ // Intel 32-bit
     //asm("add %1, %0" : "=r" (result) : "r" (x), "0" (y));
-    return x + y
+    return x + y;
     #elif __x86_64__ // Intel 64-bit
     asm("add %1, %0" : "=r" (result) : "r" (x), "0" (y));
     #elif __arm__ // ARM 32-bit
