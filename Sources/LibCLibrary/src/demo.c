79355801a1535fb15af1216a71a1b74eeccc8071
marcprux <mwp1@cornell.edu>
2024-01-12 22:05:40 -0500

    Update assembly sample

--- Sources/LibCLibrary/src/demo.c
+++ Sources/LibCLibrary/src/demo.c
@@ -19,7 +19,8 @@ double demo_compute(int n, double a, double b) {
 long add_with_assembly(long x, long y) {
     long result;
     #ifdef __i386__ // Intel 32-bit
-    asm("add %1, %0" : "=r" (result) : "r" (x), "0" (y));
+    //asm("add %1, %0" : "=r" (result) : "r" (x), "0" (y));
+    return x + y
     #elif __x86_64__ // Intel 64-bit
     asm("add %1, %0" : "=r" (result) : "r" (x), "0" (y));
     #elif __arm__ // ARM 32-bit
